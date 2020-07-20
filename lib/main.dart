9ac7d241acd885ad027a144416bbd4d40ca41f13
jimschenchen <jimschenchen@163.com>
2020-07-19 23:35:14 -0400

    connect to android phone

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "?",
+      // app name
       home: RandomWords(),
       theme: ThemeData(
         primaryColor: Colors.white,
